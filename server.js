7434d200ff1b7b511a4e5306a4c1f27e1bf08855
Ryan Tanay <ryantanay@newevolution.org>
2016-02-22 11:16:50 -0600

    Added in redis for storing session information, made middleware logging of session also include # of hits per page

--- server.js
+++ server.js
@@ -3,17 +3,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
 const localPort = 3000;
 const port = process.env.PORT || localPort;
 const secret = process.env.SECRET || 'DevPasswordYo';
 
 const app = express();
 app.set('view engine', 'jade');
-app.use(session({secret: secret}));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: secret,
+  store: new RedisStore()
+}));
 app.use((req, res, next) => {
-  req.session.count = req.session.count || 0; // eslint-disable-line no-magic-numbers
-  ++req.session.count;
+  req.session.visits = req.session.visits || {};
+  req.session.visits[req.url] = req.session.visits[req.url] || 0; // eslint-disable-line no-magic-numbers
+  ++req.session.visits[req.url];
   console.log(req.session); // eslint-disable-line no-console
   next();
 });
