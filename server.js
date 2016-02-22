5267fbe86b5901d34e61ff40513164c45bd63f46
Ryan Tanay <ryantanay@newevolution.org>
2016-02-22 11:52:08 -0600

    Pulled routes out into user routes, added mongodb for user storage

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
+const userRoutes = require('./lib/user/routes');
+
 const localPort = 3000;
+const localMongoPort = 27017;
 const port = process.env.PORT || localPort;
 const secret = process.env.SECRET || 'DevPasswordYo';
+const MONGO_PORT = process.env.MONGO_PORT || localMongoPort;
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+const MONGO_USER = process.env.MONGO_USER || '';
+const MONGO_PASS = process.env.MONGO_PASS || '';
+const MONGO_AUTH = MONGO_USER ? `${MONGO_USER}:${MONGO_PASS}@` : '';
+const MONGO_URL = `mongodb://${MONGO_AUTH}${MONGO_HOST}:${MONGO_PORT}/nodeauth`;
 
 const app = express();
 app.set('view engine', 'jade');
@@ -28,33 +38,17 @@ const date = new Date();
 app.locals.title = 'NodeAuth';
 app.locals.year = date.getFullYear()
 
+app.use(userRoutes);
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
+mongoose.connect(MONGO_URL);
 
-app.post('/login', (req, res) => {
-  res.redirect('/');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.post('/register', (req, res) => {
-  if (req.body.password === req.body.repassword) {
-    res.redirect('/login');
-  } else {
-    res.render('register', {
-      email: req.body.email,
-      message: 'Passwords do not match'
-    });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`); // eslint-disable-line no-console
+mongoose.connection.on('open', err => {
+  if (err) throw err;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`); // eslint-disable-line no-console
+  })
 });
