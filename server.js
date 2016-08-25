7d935756645182920ddef1f20526d01aefc38692
Ryan Tanay <ryantanay@newevolution.org>
2016-08-25 10:14:33 -0500

    Stripped eslint disable due to new ruleset excluding common index numbers as non-magic.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(passport.session());
 // Add visit information to session object
 app.use((req, res, next) => {
   req.session.visits = req.session.visits || {};
-  req.session.visits[req.url] = req.session.visits[req.url] || 0; // eslint-disable-line no-magic-numbers
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
   ++req.session.visits[req.url];
   next();
 });
